9a852769a71f5d5309b8fcfad4b23e365ebd2828
Radhik Sapalapalli <244g1a05v3@srit.ac.in>
2026-08-23 17:57:34 +0530

    Time: 8 ms (44.36%), Space: 46.1 MB (95.33%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        boolean[] used =new boolean[magazine.length()];
+        for(int i=0;i<ransomNote.length();i++){
+            boolean found=false;
+            for(int j=0;j<magazine.length();j++){
+                if(ransomNote.charAt(i)==magazine.charAt(j)&&!used[j]){
+                   used[j]=true;
+                   found=true;
+                   break;
+                }
+            }
+            if(!found){
+                return false;
+            }
+        }
+        return true;
+    }
+}
